ea3839e008493b50ed38c71d8edeb9842c1142d8
Andrei Toma <andrei.toma8@yahoo.com>
2024-05-28 12:28:30 +0300

    add link fee payment

--- contracts/GatewayA.sol
+++ contracts/GatewayA.sol
@@ -4,35 +4,55 @@ pragma solidity ^0.8.24;
 import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
 import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "hardhat/console.sol";
 
 contract GatewayA is CCIPReceiver {
+    enum PayFeesIn {
+        Native,
+        LINK
+    }
+
     string public data = "Empty message";
 
-    constructor(address router) CCIPReceiver(router) {}
+    IERC20 linkToken;
+
+    constructor(address _router, IERC20 _linkToken) CCIPReceiver(_router) {
+        linkToken = _linkToken;
+    }
 
     function sendMessage(
         uint64 _destinationChainSelector,
         address _destinationContract,
-        string calldata _message
+        string calldata _message,
+        PayFeesIn _payFeesIn
     ) external payable {
         // Send a message to another chain
         Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
             receiver: abi.encode(_destinationContract),
             data: abi.encode(_message),
             tokenAmounts: new Client.EVMTokenAmount[](0),
-            feeToken: address(0),
-            extraArgs: Client._argsToBytes(
-                Client.EVMExtraArgsV1({gasLimit: 1000000})
-            )
+            feeToken: _payFeesIn == PayFeesIn.LINK
+                ? address(linkToken)
+                : address(0),
+            extraArgs: ""
         });
 
         IRouterClient router = IRouterClient(i_ccipRouter);
 
         uint256 fee = router.getFee(_destinationChainSelector, message);
 
-        router.ccipSend{value: fee}(_destinationChainSelector, message);
+        console.log(linkToken.balanceOf(address(this)));
+        console.log(fee);
+        console.log(linkToken.allowance(address(this), address(router)));
+
+        if (_payFeesIn == PayFeesIn.LINK) {
+            linkToken.approve(address(router), fee);
+            router.ccipSend(_destinationChainSelector, message);
+        } else {
+            router.ccipSend{value: fee}(_destinationChainSelector, message);
+        }
     }
 
     function _ccipReceive(
--- contracts/GatewayB.sol
+++ contracts/GatewayB.sol
@@ -4,13 +4,29 @@ pragma solidity ^0.8.24;
 import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
 import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "hardhat/console.sol";
 
 contract GatewayB is CCIPReceiver {
+    enum PayFeesIn {
+        Native,
+        LINK
+    }
+
+    PayFeesIn public feeType;
+
+    IERC20 linkToken;
+
     string public data = "Empty message";
 
-    constructor(address router) CCIPReceiver(router) {}
+    constructor(address router, IERC20 _linkToken) CCIPReceiver(router) {
+        linkToken = _linkToken;
+    }
+
+    function setFeeType(PayFeesIn _payFeesIn) external {
+        feeType = _payFeesIn;
+    }
 
     function _ccipReceive(
         Client.Any2EVMMessage memory incomingMessage
@@ -21,10 +37,10 @@ contract GatewayB is CCIPReceiver {
             receiver: incomingMessage.sender,
             data: incomingMessage.data,
             tokenAmounts: new Client.EVMTokenAmount[](0),
-            feeToken: address(0),
-            extraArgs: Client._argsToBytes(
-                Client.EVMExtraArgsV1({gasLimit: 1000000})
-            )
+            feeToken: feeType == PayFeesIn.LINK
+                ? address(linkToken)
+                : address(0),
+            extraArgs: ""
         });
 
         IRouterClient router = IRouterClient(i_ccipRouter);
@@ -34,10 +50,22 @@ contract GatewayB is CCIPReceiver {
             outgoingMessage
         );
 
-        router.ccipSend{value: fee}(
-            incomingMessage.sourceChainSelector,
-            outgoingMessage
-        );
+        console.log(linkToken.balanceOf(address(this)));
+        console.log(fee);
+        console.log(linkToken.allowance(address(this), address(router)));
+
+        if (feeType == PayFeesIn.LINK) {
+            linkToken.approve(address(router), fee);
+            router.ccipSend(
+                incomingMessage.sourceChainSelector,
+                outgoingMessage
+            );
+        } else {
+            router.ccipSend{value: fee}(
+                incomingMessage.sourceChainSelector,
+                outgoingMessage
+            );
+        }
     }
 
     receive() external payable {}
